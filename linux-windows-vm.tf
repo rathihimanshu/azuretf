04e5ef85084419752e47f45842345fd7e9f784a8
Himanshu <rathihimanshu@live.com>
2025-07-06 10:37:02 +0530

    Create linux-windows-vm.tf

---
+++ linux-windows-vm.tf
@@ -0,0 +1,161 @@
+# Resource Group
+resource "azurerm_resource_group" "example" {
+  name     = "rg-vm-example"
+  location = "East US"
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Network Security Group (NSG)
+resource "azurerm_network_security_group" "nsg" {
+  name                = "vm-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# NSG Association
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# # Public IPs
+# resource "azurerm_public_ip" "linux" {
+#   name                = "linux-pip"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+#   allocation_method   = "Dynamic"
+#   sku = "Basic"
+# }
+
+# resource "azurerm_public_ip" "windows" {
+#   name                = "windows-pip"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+#   allocation_method   = "Dynamic"
+#   sku = "Basic"
+# }
+
+# NICs
+resource "azurerm_network_interface" "linux_nic" {
+  name                = "linux-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    # public_ip_address_id         = "Dynamic"
+  }
+}
+
+resource "azurerm_network_interface" "windows_nic" {
+  name                = "windows-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    # public_ip_address_id          = azurerm_public_ip.windows.id
+  }
+}
+
+# Linux VM
+resource "azurerm_linux_virtual_machine" "linux_vm" {
+  name                = "linux-vm"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  size                = "Standard_B1s"
+  admin_username      = "linuxadmin"
+  admin_password      = "LinuxPa$$w0rd123!" # Minimum password complexity required
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.linux_nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "linux-osdisk"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+    }
+
+}
+
+# Windows VM
+resource "azurerm_windows_virtual_machine" "windows_vm" {
+  name                = "windows-vm"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  size                = "Standard_B2ms"
+  admin_username      = "winadmin"
+  admin_password      = "WindowsPa$$w0rd123!"
+
+  network_interface_ids = [
+    azurerm_network_interface.windows_nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "windows-osdisk"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
+
+
+
